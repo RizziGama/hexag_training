29c3ecf54be2014708af71b0efdfc18ab0538de0
Rizzi <you@example.com>
2024-05-13 16:28:28 -0300

    first training

---
+++ src/main/java/com/itau/hexagtraining/adapters/out/FindAddressByZipCodeAdapter.java
@@ -0,0 +1,21 @@
+package com.itau.hexagtraining.adapters.out;
+
+import com.itau.hexagtraining.adapters.out.client.FindAddressByZipCodeClient;
+import com.itau.hexagtraining.application.core.domain.Address;
+import com.itau.hexagtraining.application.ports.out.FindAddressByZipCodeOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FindAddressByZipCodeAdapter implements FindAddressByZipCodeOutputPort {
+
+    @Autowired
+    private FindAddressByZipCodeClient findAddressByZipCodeClient;
+
+    @Override
+    public Address find(String zipCode) {
+        var addressResponse = findAddressByZipCodeClient.find(zipCode);
+
+        return null;
+    }
+}
---
+++ src/main/java/com/itau/hexagtraining/adapters/out/client/mapper/AddressResponseMapper.java
@@ -0,0 +1,5 @@
+package com.itau.hexagtraining.adapters.out.client.mapper;
+
+
+public interface AddressResponseMapper {
+}
